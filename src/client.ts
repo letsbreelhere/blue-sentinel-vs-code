49c9f0513478855ccf1195e4f7806f85a6a7350b
letsbreelhere <letsbreelhere@gmail.com>
2024-01-07 21:56:48 -0800

    Building client messages

--- src/client.ts
+++ src/client.ts
@@ -1,43 +1,111 @@
 import * as vscode from 'vscode';
 import { window, env } from 'vscode';
 import console from './logger';
+import { messageEnum, ProtocolMessage, VSCODE_AGENT } from './protocol';
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 const WebSocket = require('ws');
 
 class Client {
-    websocket: typeof WebSocket;
-    isHost: boolean;
+  websocket: typeof WebSocket;
+  isHost: boolean;
 
-    static sockets = new Map<string, Client>();
+  static sockets = new Map<string, Client>();
 
-    static async create(documentUri: string, url: URL, isHost: boolean) {
-        const client = new Client(url, isHost);
-        Client.sockets.set(documentUri, client);
-        return client;
-    }
+  static async create(documentUri: string, url: URL, isHost: boolean) {
+  const client = new Client(url, isHost);
+  Client.sockets.set(documentUri, client);
+  return client;
+  }
 
-    constructor(url: URL, isHost: boolean) {
-        const ws = new WebSocket(url, 'chat');
-        this.websocket = ws;
-        this.isHost = isHost;
+  async sendMessage(messageType: ProtocolMessage, ...data: any) {
+  return this.websocket.send(JSON.stringify([messageEnum(messageType), ...data]));
+  }
 
-        ws.on('open', () => {
-            console.log('Client connected');
-        });
+  /*
+    The info message by the client when it first connects.
 
-        ws.on('message', (data: any) => {
-            console.log(`received: ${data}`);
-        });
+    [
+      MSG_INFO, // message type [integer]
+      session_share, // client request session share? [boolean]
+      username, // client name [string]
+      agent, // client agent [integer]
+    ]
+  */
+  async sendInfo() {
+    return this.sendMessage(
+      'MSG_INFO',
+      false, // session_share is not implemented
+      vscode.workspace.getConfiguration('instant-code').get('username'),
+      VSCODE_AGENT
+    );
+  }
 
-        ws.on('error', (error: any) => {
-            console.log(`WebSocket error: ${error}`);
-        });
+  /*
+    The initial message sent by a client to set the initial data in buffers.
+    [
+      MSG_INITIAL, // message type [integer]
+            buffer_name, // [string]
+            [
+                bufnr, // buffer number in creator client [integer] - **arbitrary in vscode**
+                client_id // client id [integer]
+            ],  // buffer unique identifier
 
-        ws.on('close', () => {
-            console.log('WebSocket connection closed');
-        });
-    }
+            pids, // list of pids of the initial content [integer[]]
+            content, // list of lines with the initial content [string[]]
+    ]
+  */
+  async sendInitialBuffer() {
+  }
+
+  /*
+    The request message. This is sent when a client **joins** a server. It asks for current data. The server relays this message to an already connected client.
+
+    [
+      MSG_REQUEST, // message type [integer]
+    ]
+  */
+  async requestInitialBuffer() {
+    return this.sendMessage('MSG_REQUEST');
+  }
+
+  /*
+    The text message. It describes individual character operation.
+
+    [
+      MSG_TEXT, // message type [integer]
+            op, // text character operation [operation]
+            [
+                bufnr, // buffer number in creator client [integer]
+                client_id // client id [integer]
+            ],  // buffer unique identifier
+            client_id, // client id of sender [integer]
+    ]
+  */
+  async sendTextOperation() {
+  }
+
+  constructor(url: URL, isHost: boolean) {
+  const ws = new WebSocket(url, 'chat');
+  this.websocket = ws;
+  this.isHost = isHost;
+
+  ws.on('open', () => {
+    console.log('Client connected');
+  });
+
+  ws.on('message', (data: any) => {
+    console.log(`received: ${data}`);
+  });
+
+  ws.on('error', (error: any) => {
+    console.log(`WebSocket error: ${error}`);
+  });
+
+  ws.on('close', () => {
+    console.log('WebSocket connection closed');
+  });
+  }
 }
 
 export default Client;
---
+++ src/protocol.ts
@@ -0,0 +1,44 @@
+export type ProtocolMessage = 'MSG_TEXT' | 'MSG_AVAILABLE' | 'MSG_REQUEST' | 'MSG_INITIAL' | 'MSG_INFO' | 'MSG_CONNECT' | 'MSG_DISCONNECT';
+
+const MESSAGE_TYPES = {
+  MSG_TEXT: 1,
+  MSG_AVAILABLE: 2,
+  MSG_REQUEST: 3,
+  MSG_INFO: 5,
+  MSG_INITIAL: 6,
+  MSG_CONNECT: 7,
+  MSG_DISCONNECT: 8,
+};
+
+export function messageEnum(message: ProtocolMessage): number {
+  return MESSAGE_TYPES[message];
+}
+
+export const VSCODE_AGENT = 1;
+
+/*
+    An operation can be an character insert operation.
+
+    [
+      OP_INS, // operation type [integer]
+      c, // character to insert [string]
+      new_pid, // pid of inserted character [pid]
+    ]
+
+    An operation can be an character delete operation.
+
+    [
+      OP_DEL, // operation type [integer]
+      c, // character to delete [string]
+      del_pid, // pid of character to delete [pid]
+    ]
+*/
+
+const OP_INS = 2;
+const OP_DEL = 1;
+
+export function operationEnum(operation: 'OP_INS' | 'OP_DEL'): number {
+  return operation === 'OP_INS' ? OP_INS : OP_DEL;
+}
+
+export type ProtocolOperation = ['OP_INS', string, number] | ['OP_DEL', string, number];
